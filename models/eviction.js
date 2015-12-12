854224f61c3f061523f7108c8b06015a6a9a73a2
isangieri <isabelleangieri@gmail.com>
2015-12-12 10:59:17 -0800

    correcting model info

---
+++ models/eviction.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var notice = require('./notice');
+
+var EvictionSchema = new Schema ({
+  address: String,
+  notice: [NoticeSchema]
+});
+
+var Eviction = mongoose.model('Eviction', EvictionSchema);
+
+module.exports = Eviction;
