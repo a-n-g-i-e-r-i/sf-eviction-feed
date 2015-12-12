8de52e5ba302ecb525ff7ea21c79d9b7a60b7fb9
isangieri <isabelleangieri@gmail.com>
2015-12-12 11:02:58 -0800

    animated single page app navigation, hardcoded data to renderEviction function

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,94 @@
 $(document).ready(function() {
   console.log('app is on!');
 
+// #########################
+// retrieve endpoint data
+// #########################
+
+// *************
+// read all evictions
+// *************
+var evictionList = [];
+evictionList.push({
+  address: '100 Broadway Ave',
+});
+
+renderEviction(evictionList[0]);
+
+// *************
+// get eviction by id
+// *************
+
+// *************
+// create notice by eviction id
+// *************
+
+
+// modal fields update form to server 
+
+
+//render eviction
+function renderEviction(eviction) {
+  console.log('rendering eviction');
+
+  var evictionHtml = 
+  "        <!-- one eviction -->" +
+  "        <div class='row album' data-eviction-id='" + 'EVICTION ID' + "'>" +
+  "          <div class='col-md-10 col-md-offset-1'>" +
+  "            <div class='panel panel-default'>" +
+  "              <div class='panel-body'>" +
+  "              <!-- begin eviction internal row -->" +
+  "                <div class='row'>" +
+  "                  <div class='col-md-9 col-xs-12'>" +
+  "                    <ul class='list-group'>" +
+  "                      <li class='list-group-item'>" +
+  "                        <h4 class='inline-header'>Address:</h4>" +
+  "                        <span class='eviction-address'>" + eviction.address + "</span>" +
+  "                      </li>" +
+                          'buildNoticeHtml(eviction.notice)' +
+  "                    </ul>" +
+  "                  </div>" +
+  "                </div>" +
+  "                <!-- end of eviction internal row -->" +
+
+  "              </div>" + // end of panel-body
+
+  "              <div class='panel-footer'>" +
+  "                <button class='btn btn-warning add-notice'>Add Notice</button>" +
+  "                <button class='btn btn-warning edit-notice'>Edit Notice</button>" +
+  "              </div>" +
+
+  "            </div>" +
+  "          </div>" +
+  "          <!-- end one eviction -->";
+
+  $('#evictions').prepend(evictionHtml);
+}
+
+//render notice
+
+//render resource
+
+// #########################
+// SPA Navigation
+// #########################
+
+$(".nav-tenants-rights").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".tenants-rights").offset().top
+    }, 500);
+});
+
+$(".nav-eviction-feed").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".eviction-feed").offset().top
+    }, 500);
+});
+
+$(".nav-view-map").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".view-map").offset().top
+    }, 500);
+});
+
 });
