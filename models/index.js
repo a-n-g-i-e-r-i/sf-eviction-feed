f5259d0410cac95d2c004b212f779d4706ed23da
isangieri <isabelleangieri@gmail.com>
2016-01-24 22:18:42 -0800

    removed resource model

--- models/index.js
+++ models/index.js
@@ -5,8 +5,6 @@ mongoose.connect(process.env.MONGOLAB_URI ||
 
 var Eviction = require('./eviction.js');
 var Notice = require('./notice.js');
-var Resource = require('./resource.js');
 
 module.exports.Eviction = Eviction;
 module.exports.Notice = Notice;
-module.exports.Resource = Resource;
--- models/notice.js
+++ models/notice.js
@@ -1,10 +1,6 @@
 var mongoose = require("mongoose");
   Schema = mongoose.Schema;
 
-//requires Resource from resource.js
-var Resource = require('./resource.js');
-var Eviction = require('./eviction.js');
-
 //defines noticeSchema
 var noticeSchema = new Schema({
   eviction_id: String,
@@ -13,7 +9,6 @@ var noticeSchema = new Schema({
   comment: String,
   notice_date: String,
   date: String,
-  resource: [Resource.schema],
 });
 
 //defines Notice by noticeSchema
