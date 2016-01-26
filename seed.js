b4fe37cd6402834d515820c09b6ea51f3cf6406f
isangieri <isabelleangieri@gmail.com>
2016-01-26 14:34:52 -0800

    updated seed file to match evictionSchema and noticeSchema

--- seed.js
+++ seed.js
@@ -7,7 +7,8 @@ evictionList.push({
   supervisor_district: '1',
   filed_on: '2015-10-29T00:00:00',
   neighborhood: 'Mission',
-  lat_lng: [37.7932081771262,37.7932081771262]
+  lat_lng: [37.7932081771262,37.7932081771262],
+  img_url: 'http://images.fineartamerica.com/images/artworkimages/mediumlarge/1/in-the-moment-square-sunset-gina-de-gorna.jpg'
 });
 
 var noticeList = [];
@@ -16,25 +17,12 @@ noticeList.push({
   title: 'Ellis Act kick out',
   user: 'riotgrrrl212',
   comment: 'Another one bites the dust',
-  date: '10/10/2015',
-  resource: '',
-});
-
-var sampleResource = [];
-sampleResource.push({
-  title: 'My First Eviction Notice',
-  link: 'http://www.socketsite.com/wp-content/uploads/2009/03/1240-5th-Avenue.jpg',
-  caption: 'I have to leave',
-  date: '10/11/2015',
-  type: 'image',
+  notice_date: '10/10/2015',
+  date: '10/10/2015'
 });
 
 evictionList.forEach(function(eviction){
   eviction.notice = noticeList;
-});
-
-noticeList.forEach(function(notice){
-  notice.resource = sampleResource;
 });
 
 db.Eviction.remove({}, function (err, evictions) {
@@ -43,6 +31,17 @@ db.Eviction.remove({}, function (err, evictions) {
     if (err) { return console.log('ERROR', err); }
     console.log('all evictions:', evictions);
     console.log('created', evictions.length, 'evictions');
+    process.exit();
+  });
+
+});
+
+db.Notice.remove({}, function (err, evictions) {
+
+  db.Notice.create(noticeList, function(err, notices) {
+    if (err) { return console.log('ERROR', err); }
+    console.log('all notices:', notices);
+    console.log('created', notices.length, 'notices');
     process.exit();
   });
 
